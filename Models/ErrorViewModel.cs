6660ea6dddfef6f1737307650ae281d92113fa02
emocanmimocan <emintrhack@gmail.com>
2020-05-14 22:04:19 +0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Electronic_Products_Market_Database_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
