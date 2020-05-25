9c8d8f9ff84a36cf8f2675035f3a5eb7189cc416
emocanmimocan <emintrhack@gmail.com>
2020-05-25 21:17:35 +0300

    publishing

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Electronic_Products_Market_Database_Management_System.Data;
 using Electronic_Products_Market_Database_Management_System.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -14,52 +15,67 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Electronic_Products_Market_Database_Management_System {
-      public class Startup {
-            public Startup (IConfiguration configuration) {
+namespace Electronic_Products_Market_Database_Management_System
+{
+      public class Startup
+      {
+            public Startup(IConfiguration configuration)
+            {
                   Configuration = configuration;
             }
 
             public IConfiguration Configuration { get; }
 
             // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices (IServiceCollection services) {
+            public void ConfigureServices(IServiceCollection services)
+            {
 
-                  services.AddEntityFrameworkNpgsql ()
-                        .AddDbContext<ApplicationDbContext> (options => options.UseNpgsql (Configuration.GetConnectionString ("DefaultConnection")));
+                  services.AddEntityFrameworkNpgsql()
+                        .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
-                  services.AddDefaultIdentity<ApplicationUser> (
-                              // options => options.SignIn.RequireConfirmedAccount = true
+                  services.AddDefaultIdentity<ApplicationUser>(
+                        // options => options.SignIn.RequireConfirmedAccount = true
                         )
-                        .AddEntityFrameworkStores<ApplicationDbContext> ();
+                        .AddEntityFrameworkStores<ApplicationDbContext>();
 
-                  services.AddControllersWithViews ();
-                  services.AddRazorPages ();
+                  services.AddHttpsRedirection(options =>
+                  {
+                        options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                        options.HttpsPort = 5001;
+                  });
+
+                  services.AddControllersWithViews();
+                  services.AddRazorPages();
             }
 
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-                  if (env.IsDevelopment ()) {
-                        app.UseDeveloperExceptionPage ();
-                        app.UseDatabaseErrorPage ();
-                  } else {
-                        app.UseExceptionHandler ("/Home/Error");
-                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                        app.UseHsts ();
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                  if (env.IsDevelopment())
+                  {
+                        app.UseDeveloperExceptionPage();
+                        app.UseDatabaseErrorPage();
                   }
-                  app.UseHttpsRedirection ();
-                  app.UseStaticFiles ();
+                  else
+                  {
+                        app.UseExceptionHandler("/Home/Error");
+                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                        app.UseHsts();
+                  }
+                  app.UseHttpsRedirection();
+                  app.UseStaticFiles();
 
-                  app.UseRouting ();
+                  app.UseRouting();
 
-                  app.UseAuthentication ();
-                  app.UseAuthorization ();
+                  app.UseAuthentication();
+                  app.UseAuthorization();
 
-                  app.UseEndpoints (endpoints => {
-                        endpoints.MapControllerRoute (
+                  app.UseEndpoints(endpoints =>
+                  {
+                        endpoints.MapControllerRoute(
                               name: "default",
                               pattern: "{controller=Product}/{action=Index}/{id?}");
-                        endpoints.MapRazorPages ();
+                        endpoints.MapRazorPages();
                   });
             }
       }
